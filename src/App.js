414a47d949c7e0166cc03c1c7a46e3b5e23a64ec
Vishwkant <62985421+vishwkant0693@users.noreply.github.com>
2024-04-01 14:51:38 +0530

    Update App.js
    trying vancel hosting with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
 import Footer from './components/header&footer/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login/Login';
 import Main from './components/Main/Main';
 
 function App() {
   return (
     <>
-      {/* <Header/>
-      <Hero/>
-      <Main /> */}
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Main />} />
+          <Route path='/sign-in' element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
